fdae9e4b05a59702ee5c17f049c297f9b7645f75
Ben Wanicur <bwanicur@gmail.com>
2018-12-04 13:26:25 -0800

    temp

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,5 +1,6 @@
 class UserTask < ApplicationRecord
   validates :user_id, :task_id, presence: true
+  validates ::user_id, scope: :task_id
 
   belongs_to :user #FK is in the users_tasks table --> user_tasks.user_id
   belongs_to :task  #FK is in the users_tasks table --> user_tasks.task_id
